8d769b93eea779c206fe571040684865e14f4e98
Administrator <Administrator@WIN-FVKOQV8MTTV>
2017-08-18 04:13:39 -0700

    [Tolulope Awode] Messaging Utilities for both SMS and Email developed..

---
+++ PonziBussinessLogic/Utitlity/EmailUtility.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PonziBussinessLogic.Utitlity
+{
+    public class EmailUtility
+    {
+
+        public string RecepientEmail { get; set; }
+        public string Subject { get; set; }
+        public string EmailBody { get; set; }
+        public string EmailSource { get; set; }
+        public string HostAddress { get; set; }
+        public bool SSLEnabled { get; set; }
+        public string Password { get; set; }
+        public int HostPort { get; set; }
+        public bool SendEmail(bool IsHTMLBody)
+        {
+            bool ok = false;
+            try
+            {
+
+                using (MailMessage mailMessage = new MailMessage())
+                {
+
+                    mailMessage.From = new MailAddress(EmailSource);
+                    mailMessage.Subject = Subject;
+                    mailMessage.Body = EmailBody;
+                    mailMessage.IsBodyHtml = IsHTMLBody;
+                    mailMessage.To.Add(new MailAddress(RecepientEmail));
+
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = HostAddress;
+                    //smtp.EnableSsl = true;
+                    System.Net.NetworkCredential NetworkCred = new System.Net.NetworkCredential();
+                    NetworkCred.UserName = EmailSource;
+                    NetworkCred.Password = "Tolulope890@";
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = NetworkCred;
+                    smtp.Port = HostPort;
+
+
+                    smtp.Send(mailMessage);
+                    smtp.Timeout = 900000000;
+                    ok = true;
+
+
+
+
+                }
+            }
+            catch
+            {
+                ok = false;
+            }
+            return ok;
+        }
+        public bool SendMail(bool IsHTMLBody, byte[] attachment, string docDescription)
+        {
+            bool ok = false;
+            try
+            {
+
+                using (MailMessage mailMessage = new MailMessage())
+                {
+
+                    mailMessage.From = new MailAddress(EmailSource);
+                    mailMessage.Subject = Subject;
+                    mailMessage.Body = EmailBody;
+                    mailMessage.IsBodyHtml = IsHTMLBody;
+                    mailMessage.To.Add(new MailAddress(RecepientEmail));
+
+                    MemoryStream m = new MemoryStream(attachment);
+                    Attachment firstAttachment = new Attachment(m, string.Format("{0}.pdf", docDescription));
+
+                    mailMessage.Attachments.Add(firstAttachment);
+
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = HostAddress;
+                    smtp.EnableSsl = SSLEnabled;
+                    System.Net.NetworkCredential NetworkCred = new System.Net.NetworkCredential();
+                    NetworkCred.UserName = EmailSource;
+                    NetworkCred.Password = Password;
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = NetworkCred;
+                    smtp.Port = HostPort;
+                    smtp.Send(mailMessage);
+                    smtp.Timeout = 90000000;
+                    ok = true;
+                }
+            }
+            catch
+            {
+                ok = false;
+            }
+            return ok;
+        }
+    }
+}
+
---
+++ PonziBussinessLogic/Utitlity/SMSUtility.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PonziBussinessLogic.Utitlity
+{
+    public class SMSUtility
+    {
+
+        public string PhoneNo { get; set; }
+        public string Message { get; set; }
+        public string APIAddress { get; set; }
+        public string EmailSource { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string MessageHeader { get; set; }
+
+        public bool SendSMS()
+        {
+            try
+            {
+                string msg = string.Format("{0}?username={1}&password={2}&sender={3}&recipient={4}&message={5}&", APIAddress, Username, Password, MessageHeader, PhoneNo, Message);
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(msg);
+                request.Timeout = 900000;
+                var response = (HttpWebResponse)request.GetResponse();
+                string m = response.StatusDescription;
+                response.Close();
+                bool bbb = false;
+                if (m.ToLower() == "ok")
+                {
+                    bbb = true;
+                }
+                return bbb;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
+
